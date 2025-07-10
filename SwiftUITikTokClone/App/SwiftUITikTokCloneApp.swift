ec99f0bad73b94421af168465acf2e5c41457478
JMKim <tdcian71@gmail.com>
2025-07-10 16:13:15 +0900

    made project base and configure tab bars

---
+++ SwiftUITikTokClone/App/SwiftUITikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITikTokCloneApp.swift
+//  SwiftUITikTokClone
+//
+//  Created by 김정민 on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
