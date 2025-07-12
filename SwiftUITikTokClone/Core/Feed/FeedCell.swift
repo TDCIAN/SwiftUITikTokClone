62a9a5376186d28ef1512ce2165d81c492f948df
JMKim <tdcian71@gmail.com>
2025-07-12 22:05:40 +0900

    made feed cell

--- SwiftUITikTokClone/Core/Feed/FeedCell.swift
+++ SwiftUITikTokClone/Core/Feed/FeedCell.swift
@@ -24,15 +24,79 @@ struct FeedCell: View {
             VStack {
                 Spacer()
                 
-                VStack {
-                    Text("carlos.sainz")
-                        .fontWeight(.semibold)
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("carlos.sainz")
+                            .fontWeight(.semibold)
+                        
+                        Text("Rocket ship prepare for takeoff!!!")
+                    }
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
                     
-                    Text("Rocket ship prepare for takeoff!!!")
+                    Spacer()
+                    
+                    VStack(spacing: 28) {
+                        
+                        Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.gray)
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bookmark.fill")
+                                .resizable()
+                                .frame(width: 22, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                    
                 }
-                .foregroundStyle(.white)
-                .font(.subheadline)
+                .padding(.bottom, 80)
             }
+            .padding()
         }
     }
 }
